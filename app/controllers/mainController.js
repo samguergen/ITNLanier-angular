8687986d3195dc8245b9f97ea3dd4fdfbd5f268e
Samantha Guergenenov <samguergen@gmail.com>
2018-02-01 12:00:38 -0500

    content change part2

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -4,4 +4,9 @@ app.controller('MainController', ['$scope', function ($scope)  {
   $scope.test = "this is a test";
   
   console.log('inside main controller', $scope.test);
+  
+  
+  $scope.testFunc = function() {
+    console.log('function triggered');
+  }
 }]);
